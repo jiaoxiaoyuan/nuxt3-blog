dc1c4e273d6227cb8b99b7ab103789506c684295
MrJiao <329492979@qq.com>
2023-11-10 12:51:36 +0800

    feat(git): test(git): 🧪 测试

--- .cz-config.js
+++ .cz-config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	// 可选类型
 	types: [
-		{ value: 'feat', name: 'feat:     新功能' },
-		{ value: 'fix', name: 'fix:      修复' },
+		{ value: 'feat', name: '新功能:     新功能' },
+		{ value: 'fix', name: '修复:      修复' },
 		{ value: 'docs', name: 'docs:     文档变更' },
 		{ value: 'style', name: 'style:    代码格式(不影响代码运行的变动)' },
 		{
